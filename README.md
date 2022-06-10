a26b19fd98e66e1807692c02f84c586621acd62c
synth-hax <97899734+szumaster@users.noreply.github.com>
2022-06-10 02:04:44 +0200

    #1 [10/06/2022]

---
+++ README.md
@@ -0,0 +1,26 @@
+[![](https://i.imgur.com/m0y21EK.png "Tech Reborn")](https://github.com/techreborn/TechReborn/)
+
+[Fork](https://github.com/TechReborn/TechReborn) for project purposes, all rights belong to their creators.
+
+* * * * * * * * *
+
+# Credits
+- Modmuss50 - Lead Developer - [Github Profile](https://github.com/modmuss50)
+- Gigabit101 - Developer - [Github Profile](https://github.com/Gigabit101)
+- Prospector - Developer & Texture Artist - [Github Profile](https://github.com/Prospector)
+- Drcrazy - Developer [Github Profile](https://github.com/drcrazy)
+- Yulife - Lead Texture Artist
+- The Chisel Team - Connected Textures (_if Chisel is installed_)
+
+# Links
+- [Official Repository](https://github.com/TechReborn/TechReborn)
+- [Official Download Page](https://www.curseforge.com/minecraft/mc-mods/techreborn)
+- [Official Website & Wikipedia](https://wiki.techreborn.ovh/doku.php)
+
+# License
+Tech Reborn is licensed under the MIT license. Full license is in [**LICENSE.md**](https://github.com/szumaster/techreborn/blob/main/LICENSE.md).
+
+Assets created by Szumaster.
+Copyright © 2022 Szumaster, All rights reserved.
+
+Report any issues regarding this repository to [szumaster](https://github.com/szumaster).
---
+++ src/main/java/techreborn/client/gui/GuiChemicalProcessingUnit.java
@@ -0,0 +1,57 @@
+package techreborn.client.gui;
+
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerEntity;
+import reborncore.client.gui.builder.GuiBase;
+import reborncore.client.gui.builder.widget.GuiButtonExtended;
+import reborncore.client.gui.guibuilder.GuiBuilder;
+import reborncore.client.screen.builder.BuiltScreenHandler;
+import techreborn.blockentity.machine.multiblock.ChemicalProcessingUnitBlockEntity;
+
+public class GuiChemicalProcessingUnit extends GuiBase<BuiltScreenHandler> {
+
+	private final ChemicalProcessingUnitBlockEntity blockEntity;
+
+	public GuiChemicalProcessingUnit(int syncID, final PlayerEntity player, final ChemicalProcessingUnitBlockEntity blockEntity) {
+		super(player, blockEntity, blockEntity.createScreenHandler(syncID, player));
+		this.blockEntity = blockEntity;
+	}
+
+	@Override
+	protected void drawBackground(MatrixStack matrixStack, final float f, final int mouseX, final int mouseY) {
+		super.drawBackground(matrixStack, f, mouseX, mouseY);
+		final Layer layer = Layer.BACKGROUND;
+		// Battery slot
+		drawSlot(matrixStack, 8, 72, layer);
+		// Input slots
+		drawSlot(matrixStack, 35, 27, layer);
+		drawSlot(matrixStack, 35, 47, layer);
+		// Four output slots
+		drawOutputSlotBar(matrixStack, 78, 36, 4, layer);
+		// JEI Button
+		builder.drawJEIButton(matrixStack, this, 158, 5, layer);
+	}
+
+	@Override
+	protected void drawForeground(MatrixStack matrixStack, final int mouseX, final int mouseY) {
+		super.drawForeground(matrixStack, mouseX, mouseY);
+		final Layer layer = Layer.FOREGROUND;
+
+		builder.drawProgressBar(matrixStack, this, blockEntity.getProgressScaled(100), 100, 55, 40, mouseX, mouseY, GuiBuilder.ProgressDirection.RIGHT, layer);
+
+		if (blockEntity.isMultiblockValid()) {
+			addHologramButton(6, 4, 212, layer).clickHandler(this::onClick);
+			builder.drawHologramButton(matrixStack, this, 6, 4, mouseX, mouseY, layer);
+		}
+		else {
+			builder.drawMultiblockMissingBar(matrixStack, this, layer);
+			addHologramButton(76, 56, 212, layer).clickHandler(this::onClick);
+			builder.drawHologramButton(matrixStack, this, 76, 56, mouseX, mouseY, layer);
+		}
+		builder.drawMultiEnergyBar(matrixStack, this, 9, 19, (int) blockEntity.getEnergy(), (int) blockEntity.getMaxStoredPower(), mouseX, mouseY, 0, layer);
+	}
+
+	public void onClick(GuiButtonExtended button, Double x, Double y) {
+		blockEntity.renderMultiblock ^= !hideGuiElements();
+	}
+}
